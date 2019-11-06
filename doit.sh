2fa6afab83cf1d0e4dab3ae8bb53374496d6a18b
Mathew Zaleski <matz@zaleski.ca>
2019-11-06 16:31:26 -0500

    example script for WATERLOO

--- doit.sh
+++ doit.sh
@@ -2,7 +2,16 @@
 
 TOOLS=~/git/dcs-gradapps-prefilter/
 
-set -s
+set -x
 $TOOLS/rsync-backup.sh
 $TOOLS/find-profile-data-app-numbers.sh > app_numbers
-python3 $TOOLS/eat.py app_numbers HOPKI
+set -
+
+SCHOOL=WATERLOO
+echo this is just an example of how to filter by $SCHOOL
+echo run command: python3 $TOOLS/eat.py app_numbers $SCHOOL
+
+read -p "pre-filter by $SCHOOL? (hit enter to continue)>"
+
+set -x
+python3 $TOOLS/eat.py app_numbers $SCHOOL
