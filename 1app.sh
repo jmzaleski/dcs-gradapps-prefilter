a7c6173c7af54180e4e95da8d73c5edb65f40c90
Mathew Zaleski <matz@zaleski.ca>
2019-11-12 09:05:12 -0500

    prefilter just one application (by number)
    put the app number on command line or it prompts. In fact can do a
    bunch by putting them in double quotes "123 456"

---
+++ 1app.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+TOOLS=~/git/dcs-gradapps-prefilter/
+
+if test ! -z "$1"
+then
+	app_num="$1"
+	read -p "hit enter to filter by $app_num >" junk
+else
+	read -p "pre-filter enter app_num and hit enter to continue) > " app_num
+fi
+
+echo running prefilter helper filtered by $app_num ...
+
+echo python3 $TOOLS/eat.py -"$app_num" '".*"'
+
+set -x
+python3 $TOOLS/eat.py -"$app_num" '.*'
