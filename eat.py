9bf5cdef932a1411919eff78515cdb42463213b9
Mathew Zaleski <matz@zaleski.ca>
2019-11-15 18:53:33 -0500

    big change to push batch name into dcs application status field

--- eat.py
+++ eat.py
@@ -161,9 +161,12 @@ def parse_positional_args():
     parser.add_argument(
         "uni_filter_regexp", help="university to filter by"
         )
+    parser.add_argument(
+        "dcs_app_status_stem", help="stem of name that non-rejected applications will have ``dcs application status'' gradapps field"
+        )
 
     args = parser.parse_args()
-    return (args.fn_of_list_of_app_nums, args.uni_filter_regexp)
+    return (args.fn_of_list_of_app_nums, args.uni_filter_regexp,args.dcs_app_status_stem)
 
 def fn(n,nn):
     "concoct full path to transcript, cv, sop files in papers dir"
@@ -176,9 +179,15 @@ if __name__ == '__main__':
     #duplicate. sorta. so works on mac and windows laptops
     for dir in [TOOLS_DIR]:
         sys.path.append(dir)
-    (fn_app_num_list,uni_filter_regexp) = parse_positional_args()
+    (fn_app_num_list,uni_filter_regexp,dcs_app_status_stem) = parse_positional_args()
 
     completed_app_dict = completed_dict_from_applicationStatus_file(COMPLETE_FILE)
+
+    import datetime
+    now = datetime.datetime.now()
+    fn_suffix = "-%s-%s-%s_%s:%s" % ( now.year, now.month, now.day, now.hour, now.minute)
+    # DCS application status field of non-rejected apps will be set to this
+    dcs_app_status = dcs_app_status_stem + fn_suffix
 
     #out of control parm parsing. can do - or -123 or -"123 456"
     if fn_app_num_list.startswith('-'):
@@ -261,6 +270,7 @@ if __name__ == '__main__':
 
     # warning, this depends on secret knowledge of gradapps status codes
     # (the int values depend on the order of some radio button that Lloyd set up)
+    # TODO: build this from enum
     prefilter_status_map = {
         1: "Reject",
         2: "Pass-Star",
@@ -299,7 +309,21 @@ if __name__ == '__main__':
                       profile_data[GradAppsField.SGS_NUM],
                       profile_data[GradAppsField.DCS_UNION_INSTITUTION].rstrip('|')
                       )
+            
         print("===============================\n")
+        
+    def write_to_new_file(header_line, fn,dict):
+        """write all lines out to a new file name"""
+        #TODO: rename new_csv_file
+        if os.path.exists(fn):
+            os.system("mv %s %s" % (fn, "/tmp"))
+        #print("existing %s moved to /tmp" % fn)
+        with open(fn,'w') as new_file:
+            print(header_line,file=new_file)
+            for k in dict.keys():
+                line = k + "," + str(dict[k])
+                #print(line)
+                print(line,file=new_file)
 
     app_num_list = sorted(app_num_list,
                           key=lambda app_num: extract_gpa_for_sorted(app_num_to_profile_data[app_num]),
@@ -308,14 +332,17 @@ if __name__ == '__main__':
     pretty_print_app_list(app_num_to_profile_data,app_num_list,None)
 
     try:
-        response = input("prefilter above " + str(len(app_num_list)) + " applications? matching " +
-                         uni_filter_regexp + " (enter q to BAIL OUT)> ")
+        print("prefilter above " + str(len(app_num_list)) + " applications?")
+        print("matching filter:", uni_filter_regexp)
+        print("setting DCS application status field stem: " + dcs_app_status )
+        response = input("enter to continue, q to exit")
+        if response == None or (len(response) > 0 and not response.lower().startswith("y")):
+            die("actually entering any char bails out.. only hitting enter alone continues.. :)")
     except:
         response = None
-        
-    if response == None or (len(response) > 0 and not response.lower().startswith("y")):
-        print("actually entering any char bails out.. hitting enter alonw continues.. :)")
-        exit(0)
+        import traceback
+        traceback.print_exc(file=sys.stderr)
+        die("oops")        
 
     def read_query_from_input(prompt):
         "UI read a line from stdin"
@@ -333,17 +360,6 @@ if __name__ == '__main__':
             print("..eof..")
             return None
 
-    def write_to_new_file(header_line, fn,dict):
-        """write all lines out to a new file name"""
-        if os.path.exists(fn):
-            os.system("mv %s %s" % (fn, "/tmp"))
-        print("existing %s moved to /tmp" % fn)
-        with open(fn,'w') as new_file:
-            print(header_line,file=new_file)
-            for k in dict.keys():
-                line = k + "," + str(dict[k])
-                print(line)
-                print(line,file=new_file)
 
     from menu import PrefilterMenu
 
@@ -375,15 +391,24 @@ if __name__ == '__main__':
     menu = PrefilterMenu(response_code_list, menu_line_dict ,"enter a letter followed by enter> ")
 
     import uuid #universal unique resource naming thingy
-    OFN_basename = "dcs-prefilter-" + str(uuid.uuid4()) + ".csv"
+    s = str(uuid.uuid4())
+    OFN_basename = "dcs-prefilter-" + s + ".csv"
     OFN = os.path.join(OFN_DIR,OFN_basename)
+    BFN_basename = dcs_app_status + s + ".csv"
+    BFN = os.path.join(OFN_DIR,BFN_basename)
+    if os.path.exists(BFN):
+        die("Sorry, " + BFN_basename + " file already exists")
+
     assert not os.path.exists(OFN)
     write_to_new_file("testwrite",OFN,{}) #test write junk to OFN to make sure have perms and all that
+    write_to_new_file("testwrite",BFN,{}) #test write junk to OFN to make sure have perms and all that
     
     #########
     # main loop asking for decisions and writing them (paranoidly) away
     #########
     decisions = {}
+    dcs_status_map = {}
+    dcs_status_map_ix = 0
     for app_num in app_num_list:
         #concoct path of app_num "papers"
         # file-NNN-1.pdf is transcript
@@ -392,13 +417,11 @@ if __name__ == '__main__':
         transcript_fn =  fn(app_num,3)
         print(os.path.basename(sop_fn),os.path.basename(cv_fn),os.path.basename(transcript_fn))
         os.system(VIEWER  + " " + sop_fn + " " + cv_fn + " " + transcript_fn)
-        #in a different shell: echo "MAIB103GMIRPJGAX92E1RZT1Z65P900P" > /tmp/user_ref
         print('user_ref=$(cat /tmp/user_ref) && open "https://confs.precisionconference.com/~mscac20/submissionProfile?paperNumber=' + app_num +'&userRef=$user_ref"')
         resp = ""
         while True:
             print("choose dcs prefilter status for application >>>",app_num,"<<< from menu below")
-            print(app_num_to_profile_data[app_num][GradAppsField.DCS_UNION_INSTITUTION])
-            
+                        
             ########## menu for actual decision 
             resp = menu.menu()
             if resp == None:
@@ -407,7 +430,7 @@ if __name__ == '__main__':
             
             if resp.startswith('S'):
                 print("okay, skipping", app_num)
-                break ######### SORRY.. goes to next application (or once did) 
+                break ######### goto next application (or once did) 
             
             gradapps_response = gradapps_response_map[resp]
             #print("resp:", resp, gradapps_response)
@@ -415,16 +438,25 @@ if __name__ == '__main__':
             if gradapps_response == None:
                 print("gotta choose something here. looping back to same application")
                 continue
-            print(resp)
+
             try:
                 profile_data = app_num_to_profile_data[app_num]
                 decisions[profile_data[GradAppsField.SGS_NUM]] = gradapps_response
-            
+                #TODO: fix this searching through string value for state
+                if re.search("Reject", gradapps_response):
+                    print("skip adding", app_num, "to dcs_status_map because rejected")
+                else:
+                    status = "%s-%02d" % (dcs_app_status, dcs_status_map_ix)
+                    dcs_status_map[profile_data[GradAppsField.SGS_NUM]] = status
+                    write_to_new_file(dcs_app_status_stem,BFN, dcs_status_map)
+                    dcs_status_map_ix += 1
+                                
                 ########## paranoidly, write every time
                 # megaparanoid would be to copy file each time to tmp
                 write_to_new_file(uni_filter_regexp,OFN, decisions) # 
             #except:
             except Exception as e:
+                input("hello2")
                 print(e)
                 import traceback
                 traceback.print_exc(file=sys.stderr)
@@ -439,35 +471,41 @@ if __name__ == '__main__':
     #########
     # rest of script largely BS for convenience putting the output somewhere useful
     #########
-    print(decisions)
+    print("\n=========================")
     os.system("cat " + OFN)
     os.system("ls -l " + OFN)
-    print("""to import these prefilter decisions into the gradapps system:
-    1. copy to file on apps1 
-    2. curl to gradapps server""")
-    print("\n=========================\n...and execute following commands:\n")
-    dest = "%s:%s/" % (CSLAB_USERID, MSCAC_PREFILTER_DIR_NAME)
-    rsync_cmd = "rsync  %s %s" %  (OFN, dest)
+    print("""next import these prefilter decisions into the gradapps system:
+    1. copy/rsync files to apps1 
+    2. run curl commands to gradapps server to update dcs application status and prefilter status columns""")
     
-    URL='https://confs.precisionconference.com/~mscac20/uploadApps?config=prefilter&pass=StayorGo'
-    curl_cmd =  'curl -F appsFile="@mscac-prefilter/%s" "%s"' % ( OFN_basename, URL )
+    dest = "%s:%s/" % (CSLAB_USERID, MSCAC_PREFILTER_DIR_NAME)
+    rsync_cmd = "rsync  %s %s %s" %  (OFN, BFN, dest)
+
+    #magic URL's configured into gradapps to upload data into fields
+    URL_TEMPL='https://confs.precisionconference.com/~mscac20/uploadApps?config=%s&pass=StayorGo'
+    CURL_TEMPL = 'curl -F appsFile="@mscac-prefilter/%s" "%s"'
+    curl_cmd =  CURL_TEMPL % ( OFN_basename, URL_TEMPL % "prefilter" )
+    curl_dcsstatus_cmd =  CURL_TEMPL % ( BFN_basename, URL_TEMPL % "dcsstatus" )
 
     # probably will need ssh config support or will prompt for password
     print(rsync_cmd)
-    print("ssh qew", "'" + curl_cmd + "'") #gross quoting, sorry
-    print("\n=========================\n")
+    #print("ssh qew", "'" + curl_cmd + "'") #gross quoting, sorry
+    print("=========================\n")
     resp = input("hit Enter to exec rsync above (control-c only way to skip rsync) > ")
     if not resp.startswith('s'):
         os.system(rsync_cmd)
 
-    print("check that rsync'd file made it..")
-    os.system("ssh %s ls -ltr %s" % (CSLAB_USERID, MSCAC_PREFILTER_DIR_NAME))
+    print("check that rsync'd files made it..")
+    os.system("ssh %s ls -ltr %s/" % (CSLAB_USERID, MSCAC_PREFILTER_DIR_NAME))
 
-    print("\nnow ssh to", CSLAB_USERID, "and curl file to gradapps\n\n")
+    #print("\nnow ssh to", CSLAB_USERID, "and curl file to gradapps\n\n")
     ssh_cmd = "ssh -tt %s '%s'" % (CSLAB_USERID, curl_cmd )
+    ssh_dcsstatus_cmd = "ssh -tt %s '%s'" % (CSLAB_USERID, curl_dcsstatus_cmd)
     print(ssh_cmd)
+    print(ssh_dcsstatus_cmd)
     resp = input("hit enter to curl the prefilter choices to gradapps server.. > ")
     os.system(ssh_cmd)
+    os.system(ssh_dcsstatus_cmd)
 
     # something missing i think it must be the form id. fiddle more later
     # print("..or probably better if you are on a unix machine..")
