9c87d43c7a7d213e11b84c2dee02f5e23914ab63
Mathew Zaleski <matz@zaleski.ca>
2019-11-17 14:28:46 -0500

    handle lists correctly
    messed double quotes

--- 1app.sh
+++ 1app.sh
@@ -4,7 +4,7 @@ TOOLS=~/git/dcs-gradapps-prefilter/
 
 if test ! -z "$1"
 then
-	app_num="$1"
+	app_num="$*"
 	read -p "hit enter to filter by $app_num >" junk
 else
 	read -p "pre-filter enter app_num and hit enter to continue) > " app_num
