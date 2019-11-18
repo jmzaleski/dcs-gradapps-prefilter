3151f68f4ae183bc9fdf8aaf0b0082b46acfeaec
Mathew Zaleski <matz@zaleski.ca>
2019-11-18 08:54:56 -0500

    try ssh to gradapps. serves as reminder to start VPN off campus.

--- rsync-backup.sh
+++ rsync-backup.sh
@@ -4,7 +4,8 @@
 # it depends on having my ssh public key (id_rsa.pub) known by gradbackup@gradapps.cs.toronto.edu
 # lloyd will have to set this up for you.
 
-CMD="rsync -av gradbackup@gradapps.cs.toronto.edu:archive/mscac.2020/mscac20/public_html ."
+GRAD_APPS="gradbackup@gradapps.cs.toronto.edu"
+CMD="rsync -av $GRAD_APPS:archive/mscac.2020/mscac20/public_html ."
 
 if test ! -d public_html
 then
@@ -13,8 +14,12 @@ then
 	exit 2
 fi
 
-echo you will exec: $CMD
-read -p "hit enter to rsync gradapps backup to . > " JUNK
+echo trying ssh $GRAD_APPS whoami
+echo if hangs you probably need to get on CSLAB vpn..
+ssh $GRAD_APPS whoami
+
+echo okay then.. about exec: $CMD
+read -p "hit enter to continue ... > " JUNK
 set -x
 $CMD
 
