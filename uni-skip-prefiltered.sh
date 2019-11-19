e12e8d3ff885a05ddf61ebad63e12e9f172988f1
Mathew Zaleski <matz@zaleski.ca>
2019-11-19 13:34:27 -0500

    wrapper sript using new --skip-prefiltered cmd line option

---
+++ uni-skip-prefiltered.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+TOOLS=~/git/dcs-gradapps-prefilter/
+
+if test ! -z "$1"
+then
+	SCHOOL="$1"
+	echo $0 "filter by $SCHOOL"
+else
+	read -p "pre-filter by SCHOOL? (enter filter string eg TORONTO and hit enter to continue) > " SCHOOL
+fi
+
+DCS_STATUS=pf
+echo running prefilter helper filtered by $SCHOOL ...
+
+echo python3 $TOOLS/eat.py app_numbers $SCHOOL $DCS_STATUS
+
+set -x
+python3 $TOOLS/eat.py --skip-prefiltered app_numbers "$SCHOOL" $DCS_STATUS
