4d61b36e16efcab3c15df491445240f105c1dadf
Mathew Zaleski <matz@zaleski.ca>
2019-11-20 08:19:35 -0500

    comment so it more useful as doc

--- doit.sh
+++ doit.sh
@@ -12,14 +12,23 @@ else
 	SCHOOL="$new_school"
 fi
 
+# refresh local copy of gradapps from overnight backup site
+# snapshot from early morning
+
 set -x
 $TOOLS/rsync-backup.sh
+set -
+
+# create a file of app numbers to prefilter
+
+set -x
 $TOOLS/find-profile-data-app-numbers.sh > app_numbers
 set -
 
 DCS_STATUS=pf
 echo example of how to filter by $SCHOOL
 
+# run the pre-filtering helper script. This version presents only apps from $SCHOOL sorted by gpa
 echo run command: python3 $TOOLS/eat.py app_numbers $SCHOOL $DCS_STATUS
 
 set -x
