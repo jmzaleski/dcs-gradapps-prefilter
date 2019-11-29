463490b3216dabd559a45c9022e8b286578000b4
Mathew Zaleski <matz@zaleski.ca>
2019-11-29 08:41:45 -0500

    WIP big rewrite to look at creation date, author and creator

--- scan_ref_letter_pdf.py
+++ scan_ref_letter_pdf.py
@@ -1,8 +1,15 @@
+VERBOSE=False
+
+def die(*objs):
+    print("FATAL ERROR: ", *objs, file=sys.stderr)
+    exit(42)
+
+def err_msg(*objs):
+    if VERBOSE: print("ERROR: ", *objs, file=sys.stderr)
 
 import PyPDF3 #note weird name. obtained by pip3 install pypdf3
 import types # for SimpleNamespace
 
-VERBOSE=False
 
 def die(*objs):
     print("ERROR: ", *objs, file=sys.stderr)
@@ -29,18 +36,22 @@ if not os.path.exists(MSCAC_PROFILE_DATA_ROOT_DIR): die(MSCAC_PROFILE_DATA_ROOT_
     
 def get_creation_date(info):
     #from https://stackoverflow.com/questions/16503075/convert-creationtime-of-pdf-to-a-readable-format-in-python
-    if '/CreationDate' in info:
-        datestring = info['/CreationDate'][2:-7]
-    elif 'CreationDate' in info:
-        datestring = info['CreationDate'][2:-7]
-    elif '/ModDate' in info:
-        datestring = info['/ModDate'][2:-7]
-    elif 'ModDate' in info:
-        datestring = info['ModDate'][2:-7]
-    else:
-        print("no creation date in info??", path)
-        print("info",info)
+    try:
+        if '/CreationDate' in info:
+            datestring = info['/CreationDate'][2:-7]
+        elif 'CreationDate' in info:
+            datestring = info['CreationDate'][2:-7]
+        elif '/ModDate' in info:
+            datestring = info['/ModDate'][2:-7]
+        elif 'ModDate' in info:
+            datestring = info['ModDate'][2:-7]
+        else:
+            err_msg("no creation date in info??", path, info)
+            return None
+    except:
+        err_msg("info[CreationDate] deref throws?", info)
         return None
+        
     if VERBOSE: print("datestring from info['/CreationDate'][2:-7]", datestring)
     from time import mktime, strptime
     from datetime import datetime
@@ -49,59 +60,73 @@ def get_creation_date(info):
         dt = datetime.fromtimestamp(mktime(ts))
         return dt.date()
     except:
-        print("date conversion throws on", datestring)
+        err_msg("date conversion throws on", datestring)
         return None
 
 def get_creator(info):
-    if '/Creator' in info:
-        name = info['/Creator']
-    elif 'Creator' in info and len(info["Creator"])>0:
-        name = info['Creator']
-    elif 'Producer' in info:
-        name = info['Producer']
-    elif '/Producer' in info:
-        name = info['/Producer']
-    else:
-        name = None
-    if not name:
-        print("no Creator found in", info, file=sys.stderr)
+    try:
+        if '/Creator' in info:
+            name = info['/Creator']
+        elif 'Creator' in info and len(info["Creator"])>0:
+            name = info['Creator']
+        elif 'Producer' in info:
+            name = info['Producer']
+        elif '/Producer' in info:
+            name = info['/Producer']
+        else:
+            name = None
+        if not name:
+            err_msg("no Creator found in", info)
+            return None
+        else:
+            return name
+    except:
+        err_msg("info[Creator] deref throws?", info)
         return None
-    else:
-        return name
     
 def get_author(info):
-    if '/Author' in info:
-        name = info['/Author']
-    elif 'Author' in info:
-        name = info['Author']
-    else:
-        name = None
-    if not name:
-        print("no Author found in", info,file=sys.stderr)
+    try:
+        if '/Author' in info:
+            name = info['/Author']
+        elif 'Author' in info:
+            name = info['Author']
+        else:
+            name = None
+        if not name:
+            err_msg("no Author found in", info)
+            return None
+        else:
+            #print("Author",name)
+            return name
+    except:
+        err_msg("info[Author] deref throws?", info)
         return None
-    else:
-        print("Author",name)
-        return name
 
     
 def get_info_ns(path):
     with open(path, 'rb') as f:
-        my_pdf_file_reader = PyPDF3.pdf.PdfFileReader(f)
-        info = my_pdf_file_reader.getDocumentInfo()
-        if not info:
-            return None
+        try:
+            my_pdf_file_reader = PyPDF3.pdf.PdfFileReader(f)
+            info = my_pdf_file_reader.getDocumentInfo()
+            if VERBOSE:
+                print(type(info).__name__)
+                print(info)
+        except:
+            err_msg("get_info_ns failed to open PDF file:", path)
+            ns = types.SimpleNamespace()
+            ns.creator = None
+            ns.author = None
+            ns.creationdate = None
+            return ns
 
-        if VERBOSE:
-            print(type(info).__name__)
-            print(info)
         dict = {}
         dict["author"] = get_author(info)
         dict["creator"] = get_creator(info)
         dict["creationdate"] = get_creation_date(info)
         ns = types.SimpleNamespace(**dict)
-        print("ns.creator:", ns.creator)
-        print("ns.author:", ns.author)
-        print("ns.creationdate:", ns.creationdate)        
+        # print("ns.creator:", ns.creator)
+        # print("ns.author:", ns.author)
+        # print("ns.creationdate:", ns.creationdate)        
         return ns
 
 def naive_get_info(path):
@@ -125,6 +150,15 @@ def naive_get_info(path):
         print('subject:', info.subject)
         print('title:', info.title)
                     
+def pretty_print(app_num, file_list):
+    print("app_num", app_num)
+    for fn in file_list:
+        print("    %020s,%30s,%30s" % (
+                os.path.basename(fn),
+                pdf_meta_data_for_fn[fn].author,
+                pdf_meta_data_for_fn[fn].creator
+                ))
+
 if __name__ == '__main__':
     import os, sys
     path = os.path.join(MSCAC_PAPERS_DIR,'326','file326-3.pdf')
@@ -135,61 +169,122 @@ if __name__ == '__main__':
         exit(0)
 
     #traverse the directory tree and collect all the PDF files under each app
-    dict = {}
+    #gradapps keeps the review letters for a applicant in the data/app_num/reviews directory
+    # scan the files in each app and stash the PDF meta information for each one
+    
+    dict = {} # maps app_num to list of files in app_num/reviews
+    reviews_for_app_num = {} # maps app_num to list of files in app_num/reviews
+    pdf_meta_data_for_fn = {}
     for root, dirs, files in os.walk(MSCAC_PROFILE_DATA_ROOT_DIR):
         for file in files:
             if file.endswith('pdf') or file.endswith('PDF'): 
                 (dir,fn) = os.path.split(root)
                 (dir2,app_num) =os.path.split(dir)
-                print(app_num)
+                #print(app_num)
                 if not app_num in dict:
                     dict[app_num] = []
-                dict[app_num].append(os.path.join(root, file))
+                    reviews_for_app_num[app_num] = []
+                ffn = os.path.join(root, file)
+                dict[app_num].append(ffn)
+                reviews_for_app_num[app_num].append(ffn)
+                pdf_meta_data_for_fn[ffn] = get_info_ns(ffn)
 
-    #TODO: scan the files in each app and stash the PDF meta information for each one
-    
+    import datetime
+    creation_dates_match = {}
+    creator_match = {}
+    author_match = {}
+    for app_num in dict:
+        file_list = dict[app_num]
+        if len(file_list) < 2:
+            continue
+        fn0 = file_list[0]
+        cd0 = pdf_meta_data_for_fn[fn0].creationdate
+        flg = True
+        for fn in file_list:
+            #print(fn,pdf_meta_data_for_fn[fn])
+            d = pdf_meta_data_for_fn[fn].creationdate
+            if not d or pdf_meta_data_for_fn[fn].creationdate != cd0:
+                flg = False
+                break
+        if flg:
+            creation_dates_match[app_num] = app_num
+            #print("app_num", app_num, "creation dates match", cd0)
+            
+            cr0  = pdf_meta_data_for_fn[fn0].creator
+            flg2 = True
+            for fn in file_list:
+                cr = pdf_meta_data_for_fn[fn].creator
+                if not cr or pdf_meta_data_for_fn[fn].creator != cr0:
+                    flg2 = False
+                    break
+            if flg2:
+                creator_match[app_num] = app_num
+                #print("app_num", app_num, "creator matches also", cr0)
+                flg3 = True
+                a0 = pdf_meta_data_for_fn[fn].author
+                for fn in file_list:
+                    a = pdf_meta_data_for_fn[fn].author
+                    if not a or pdf_meta_data_for_fn[fn].author != a0:
+                        flg3 = False
+                        break
+                if flg3:
+                    author_match[app_num] = app_num
+                    #print("app_num", app_num, "author match", a0)
+                
+    print("creation date, creator and author match")
+    for app_num in author_match:
+        pretty_print(app_num, dict[app_num])
+    print("creation date, creator match")
+    for app_num in creator_match:
+        pretty_print(app_num, dict[app_num])
+    print("creation date match")
+    for app_num in creation_dates_match:
+        pretty_print(app_num, dict[app_num])
+
+    exit(0)
+        
     #
     # look at every file in every app and collect the creation date of each file
-    dict_dates = {}
-    for app_num in dict.keys():
-        file_list = dict[app_num]
-        if VERBOSE: print(app_num)
-        for fn in file_list:
-            try:
-                info_ns = get_info_ns(fn)
-                file_creation_date = info_ns.creationdate
-                #file_creation_date = get_info_ns(fn)
-                if file_creation_date:
-                    if not app_num in dict_dates:
-                        dict_dates[app_num] = []
-                    dict_dates[app_num].append(file_creation_date)
-                    if VERBOSE: print( app_num, os.path.basename(fn), file_creation_date)
-                else:
-                    print( app_num, os.path.basename(fn), 'NO DATE', file=sys.stderr)
-            except:
-                import traceback,sys
-                print("get_info_ns throws for", app_num, fn, file=sys.stderr)
-                traceback.print_exc(file=sys.stderr)
+    # dict_dates = {}
+    # for app_num in dict.keys():
+    #     file_list = dict[app_num]
+    #     if VERBOSE: print(app_num)
+    #     for fn in file_list:
+    #         try:
+    #             info_ns = get_info_ns(fn)
+    #             if info_ns == None:
+    #                 continue
+    #             if info_ns.creationdate:
+    #                 if not app_num in dict_dates:
+    #                     dict_dates[app_num] = []
+    #                 dict_dates[app_num].append(info_ns.creationdate)
+    #                 if VERBOSE: print( app_num, os.path.basename(fn), info_ns.creationdate)
+    #             else:
+    #                 print( app_num, os.path.basename(fn), 'NO DATE', file=sys.stderr)
+    #         except:
+    #             import traceback,sys
+    #             print("get_info_ns throws for", app_num, fn, file=sys.stderr)
+    #             traceback.print_exc(file=sys.stderr)
 
-    input('ready for date search?')
+    # input('ready for date search?')
 
-    #look at the dates and look for files that were created on same date
-    import datetime
-    for app_num in dict_dates.keys():
-        date_list = dict_dates[app_num]
-        if len(date_list) < 2:
-            continue
-        max_date = datetime.date(2018,1,1)
-        min_date = datetime.date(2020,12,12) 
-        for d in date_list:
-            if d > max_date:
-                max_date = d
-            if d < min_date:
-                min_date = d
-        if max_date == min_date:
-            for p in dict[app_num]:
-                print (app_num,os.path.basename(p))
-                #print(app_num, min_date, date_list, dict[app_num])
+    # #look at the dates and look for files that were created on same date
+    # import datetime
+    # for app_num in dict_dates.keys():
+    #     date_list = dict_dates[app_num]
+    #     if len(date_list) < 2:
+    #         continue
+    #     max_date = datetime.date(2018,1,1)
+    #     min_date = datetime.date(2020,12,12) 
+    #     for d in date_list:
+    #         if d > max_date:
+    #             max_date = d
+    #         if d < min_date:
+    #             min_date = d
+    #     if max_date == min_date:
+    #         for p in dict[app_num]:
+    #             print (app_num,os.path.basename(p))
+    #             #print(app_num, min_date, date_list, dict[app_num])
 
 
 # PDF date/time format http://www.verypdf.com/pdfinfoeditor/pdf-date-format.htm
