808042150e3cd40bd64331fbb1a1516d28c806fb
Mathew Zaleski <matz@zaleski.ca>
2019-11-28 20:36:18 -0500

    add a bunch of  US schools that cropped up this week.

---
+++ usa.py
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+TOOLS=~/git/dcs-gradapps-prefilter/
+
+SCHOOL='BERKELEY|HOPKINS|Stanford|BOSTON|GEORGIA|MIT|COLUMBIA|CORNELL|CALIFORNIA|WISCONSIN|CHAMPAIGN|MELLON|CINCINNATI|RUTGERS|SAN JOSE|TEXAS_AUSTIN|PENNSYLVANIA'
+
+
+echo
+echo default filter $SCHOOL '(ostensibly)' finds the top Canadians unis
+echo you can put this on command line or enter new one below
+echo
+
+if test ! -z "$1"
+then
+	SCHOOL="$1"
+	echo $0 "filter by $SCHOOL"
+else
+	echo you can give another regexp on command line. or enter one below, if you enter nothing assumes above
+	echo an example filter would be "$SCHOOL"
+	read -p "enter school filter.. (just enter uses above default) > " school
+	if test ! -z $school
+	then
+		SCHOOL="$school"
+	fi
+fi
+
+DCS_STATUS=pf
+
+set -x
+python3 $TOOLS/eat.py --skip-prefilter "$SCHOOL"
